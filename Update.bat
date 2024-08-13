c802db35d08929b6d68143d8ead582bfa301f339
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-13 16:40:07 +0800

    测试脚本

---
+++ Update.bat
@@ -0,0 +1,9 @@
+@echo off
+::后续命令使用的是：UTF-8编码
+chcp 65001
+:: for /F will launch a new instance of cmd so we create a guard to prevent an infnite loop
+if not defined FNM_AUTORUN_GUARD (
+	set "FNM_AUTORUN_GUARD=AutorunGuard"
+	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
+)
+hexo g && hexo d && git add . && git commit -m "测试脚本" && git push origin main
