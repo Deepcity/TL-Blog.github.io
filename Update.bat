dc2560b346d03cf4d5c50bac4a351149242b9fa7
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-14 13:38:11 +0800

    更新日志页——由Google Calcendar支持

--- Update.bat
+++ Update.bat
@@ -6,4 +6,4 @@ if not defined FNM_AUTORUN_GUARD (
 	set "FNM_AUTORUN_GUARD=AutorunGuard"
 	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
 )
-hexo clean && hexo g && hexo d && git add . && git commit -m "修复标签的不当使用" && git push origin main
+hexo clean && hexo g && hexo d && git add . && git commit -m "更新日志页——由Google Calcendar支持" && git push origin main
--- source/tags/index.md
+++ source/tags/index.md
@@ -1,5 +1,5 @@
 ---
-title: _tags
+title: 标签
 date: 2024-08-14 13:02:25
 type: tags
 ---
