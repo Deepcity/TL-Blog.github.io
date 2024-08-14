651947a3fd23afd4b3db49663945d78b94c35837
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-14 17:38:49 +0800

    SEO补充与语言更新

--- Update.bat
+++ Update.bat
@@ -6,4 +6,4 @@ if not defined FNM_AUTORUN_GUARD (
 	set "FNM_AUTORUN_GUARD=AutorunGuard"
 	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
 )
-hexo clean && hexo g && hexo d && git add . && git commit -m "服务器pull测试" && git push origin main
+hexo clean && hexo g && hexo d && git add . && git commit -m "SEO补充与语言更新" && git push origin main
