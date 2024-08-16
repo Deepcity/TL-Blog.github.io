c6d26fe8e52551b1cd3278db6ac54c7634af751b
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-16 12:43:04 +0800

    更新架构后CICD更新

--- Update.bat
+++ Update.bat
@@ -6,4 +6,4 @@ if not defined FNM_AUTORUN_GUARD (
 	set "FNM_AUTORUN_GUARD=AutorunGuard"
 	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
 )
-hexo g && hexo d && git add . && git commit -m "blog架构更新" && git push origin main
+hexo g && hexo d && git add . && git commit -m "更新架构后CICD更新" && git push origin main
