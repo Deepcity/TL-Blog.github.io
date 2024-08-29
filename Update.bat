5ff935ccaeba50de7468106389872cd641c83461
Deepcity <91273161+Deepcity@users.noreply.github.com>
2024-08-29 19:29:23 +0800

    维护Bug记录更新

--- Update.bat
+++ Update.bat
@@ -6,4 +6,4 @@ if not defined FNM_AUTORUN_GUARD (
 	set "FNM_AUTORUN_GUARD=AutorunGuard"
 	FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd') DO CALL %%z
 )
-hexo g && hexo d && git add . && git commit -m "例行维护结束" && git push origin main
+hexo g && hexo d && git add . && git commit -m "维护Bug记录更新" && git push origin main
